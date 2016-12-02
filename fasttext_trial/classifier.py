5ed0b41c3be7cd314539f0a0ec76c4c3a643efa8
DESKTOP-B15J91F\Mohammad Yaghini <mohammad.yaghini@gmail.com>
2016-12-02 18:10:06 +0100

    fasttext_trial: the labeling functions needs some work.

---
+++ fasttext_trial/classifier.py
@@ -0,0 +1,19 @@
+import fasttext
+from cleaners import *
+from preprocess import *
+
+INPUT_FILE_NAME = 'train_pos.txt'
+
+# Cleaning the data set
+
+# TODO: Removing duplicate lines
+clean_duplicates('train_pos.txt', 'no_dup.txt')
+
+# TODO: Remove <user> tags.
+clean_user_tags('no_dup.txt', 'no_user.txt')
+
+# TODO: Add _label_
+add_label('no_user.txt', 'pos', 'labeled.txt')
+
+# train classifier
+classifier = fasttext.supervised('labeled.txt', 'supervised_model')
